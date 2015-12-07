c48d49ebce5dcad7d32df5b56601ade7f60f968e
Pierrick <veran@intechinfo.fr>
2015-12-07 11:41:15 +0100

    Change files to production value

--- DOCKER/start_web.sh
+++ DOCKER/start_web.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Pierrick VERAN - Tardigrade Security Challenge - 09/11/2015
 # Launch nginx-gunicorn-django container link with tsc's webfiles
+
 docker rm -f tsc_web_storage
 
 docker run \
 --name tsc_web_storage \
+--restart=always \
  -v /home/lilyus/Git/TSC_storage/WWW:/var/www/:ro \
--p 127.0.0.1:8080:80 \
+-p 80:80 \
 -d tsc/storage:Web

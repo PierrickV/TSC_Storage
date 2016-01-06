33682df14025946c14026e67bfa2ad2e0bc58356
Pierrick <veran@intechinfo.fr>
2016-01-06 14:41:12 +0100

    Création des scripts pour gérer les challenges sur le serveur de stockage

--- DOCKER/start_web.sh
+++ DOCKER/start_web.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Pierrick VERAN - Tardigrade Security Challenge - 09/11/2015
 # Launch nginx-gunicorn-django container link with tsc's webfiles
+
 docker rm -f tsc_web_storage
 
 docker run \
---name tsc_web_storage \
- -v /home/lilyus/Git/TSC_storage/WWW:/var/www/:ro \
--p 127.0.0.1:8080:80 \
--d tsc/storage:Web
+        --name tsc_web_storage \
+         -v ~/Git/TSC_storage/WWW:/var/www/:ro \
+        -p 127.0.0.1:80:80 \
+        -d tsc/storage:Web
